4b77b0dec86a9102e068f335e362882fc30ea7a2
keithmccall <keithcodes@gmail.com>
2020-08-15 17:28:19 -0700

    Copying Css

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  presets: ["@babel/preset-env","@babel/preset-react"],
+  presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: [
     ["@babel/plugin-proposal-decorators", { legacy: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }],
+    "transform-react-remove-prop-types",
   ],
 };
